1ebc9ae5069277b2c95d3d764ff73fc32f04853b
Joao Meyer <1994meyer@gmail.com>
2018-03-07 12:34:46 -0300

    Changed program to be more mvc structured

--- main.cc
+++ main.cc
@@ -1,72 +1,23 @@
-#include <gtkmm/button.h>
 #include <gtkmm/window.h>
-#include <gtkmm/builder.h>
 #include <gtkmm/application.h>
-#include <gtkmm/drawingarea.h>
 
-#include "src/mode/draw.h"
-#include "src/mode/zoom.h"
-#include "src/mode/movement.h"
+#include "src/control.h"
+#include "src/view/view.h"
 #include "src/mode/display_file.h"
 
 
-void connect_draw(Glib::RefPtr<Gtk::Builder>& builder, DisplayFile* df) {
-    Gtk::DrawingArea* drawing_area = nullptr;
-    builder->get_widget("drawing_area", drawing_area);
-
-    auto binded = sigc::bind(&draw, df);
-    drawing_area->signal_draw().connect(binded);
-}
-
-void connect_buttons(Glib::RefPtr<Gtk::Builder>& builder, DisplayFile* df) {
-    Gtk::DrawingArea* drawing_area = nullptr;
-    builder->get_widget("drawing_area", drawing_area);
-
-    Gtk::Button* button_up = nullptr;
-    builder->get_widget("button_up", button_up);
-    auto bind_up = sigc::bind(&move_up, df, drawing_area);
-    button_up->signal_clicked().connect(bind_up);
-
-    Gtk::Button* button_down = nullptr;
-    builder->get_widget("button_down", button_down);
-    auto bind_down = sigc::bind(&move_down, df, drawing_area);
-    button_down->signal_clicked().connect(bind_down);
-
-    Gtk::Button* button_left = nullptr;
-    builder->get_widget("button_left", button_left);
-    auto bind_left = sigc::bind(&move_left, df, drawing_area);
-    button_left->signal_clicked().connect(bind_left);
-
-    Gtk::Button* button_right = nullptr;
-    builder->get_widget("button_right", button_right);
-    auto bind_right = sigc::bind(&move_right, df, drawing_area);
-    button_right->signal_clicked().connect(bind_right);
-
-    Gtk::Button* button_in = nullptr;
-    builder->get_widget("button_in", button_in);
-    auto bind_in = sigc::bind(&zoom_in, df, drawing_area);
-    button_in->signal_clicked().connect(bind_in);
-
-    Gtk::Button* button_out = nullptr;
-    builder->get_widget("button_out", button_out);
-    auto bind_out = sigc::bind(&zoom_out, df, drawing_area);
-    button_out->signal_clicked().connect(bind_out);
-}
 
 int main(int argc, char* argv[]) {
 
     auto app = Gtk::Application::create(argc, argv, "org.gtkmm.engine");
 
-    auto builder = Gtk::Builder::create();
-    builder->add_from_file("src/view/main_window.glade");
-
+    View view("src/view/main_window.glade");
     Gtk::Window* window = nullptr;
-    builder->get_widget("window_main", window);
+    view.builder->get_widget("window_main", window);
 
     DisplayFile display_file;
 
-    connect_draw(builder, &display_file);
-    connect_buttons(builder, &display_file);
+    Control control(view, display_file);
 
 	return app->run(*window);
 }
