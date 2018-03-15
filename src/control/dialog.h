0e1684073801683be3c10b85d3410146fd5b0619
Joao Meyer <1994meyer@gmail.com>
2018-03-15 20:32:54 -0300

    Dialog is now a little bit more organized

--- src/control/dialog.h
+++ src/control/dialog.h
@@ -11,8 +11,8 @@
 #include <gtkmm/button.h>
 #include <gtkmm/dialog.h>
 #include <gtkmm/builder.h>
-#include <gtkmm/textview.h>
 #include <gtkmm/drawingarea.h>
+#include <gtkmm/comboboxtext.h>
 
 #include "../mode/dot.h"
 #include "../mode/line.h"
@@ -24,11 +24,19 @@ namespace Control {
 
 class Dialog {
 public:
-    Dialog(Glib::RefPtr<Gtk::Builder>& b, std::vector<Shape>& s, std::vector<Gtk::Label*>& a)
-    : shapes(s),
-      shapes_added(a) {
+    /**
+     * @brief Constructor.
+     *
+     * @param b Builder of the GTK application. Used to get all the widgets
+     * that are related to this control.
+     * 
+     * @param s Shapes vector. Will add new shapes to this vector.
+     */
+    Dialog(Glib::RefPtr<Gtk::Builder>& b, std::vector<Shape>& s)
+    : shapes(s) {
+
         b->get_widget("dialog_input", dialog_input);
-        b->get_widget("list_shapes", list_shapes);
+        b->get_widget("combobox_shapes", combobox_shapes);
         b->get_widget("button_add_shape", button_add_shape);
         b->get_widget("button_finish", button_finish);
         b->get_widget("button_cancel", button_cancel);
@@ -36,21 +44,33 @@ public:
         b->get_widget("text_input_name", text_input_name);
         b->get_widget("text_input_x", text_input_x);
         b->get_widget("text_input_y", text_input_y);
-        b->get_widget("text_points_added", text_points_added);
+        b->get_widget("box_points_added", box_points_added);
         b->get_widget("drawing_area", drawing_area);
 
         connect_buttons();
     }
 
+    /**
+     * @brief Destructor.
+     *
+     * @details The destructor for top-level widgets must be called explicitly.
+     */
     ~Dialog() {
         delete dialog_input;
+        clear_labels();
     }
 
+    /**
+     * @brief Add point to the point_buffer.
+     *
+     * @details Will only add the point if the inputs are correct numbers that 
+     * do not throw any kind of exception when passed to std::stod().
+     */
     void add_point() {
         try {
             // Get point
             Point p = get_point();
-            points_added.push_back(p);
+            points_buffer.push_back(p);
 
             // Clear text inputs
             text_input_x->set_text("");
@@ -63,82 +83,128 @@ public:
         }
     }
 
-    void add_shape() {
-        if (points_added.size() == 0) {
-            return;
+    /**
+     * @brief Adds new shape to shapes list and shapes combobox.
+     *
+     * @details If there is no points in points_buffer or no name has been set,
+     * it won't do anything and it will return false.
+     * 
+     * @return true if the shape was added. False otherwise.
+     */
+    bool add_shape() {
+        // No points no shape
+        if (points_buffer.empty()) {
+            return false;
+        }
+
+        // No name no shape
+        std::string name = text_input_name->get_text();
+        if (name.empty()) {
+            return false;
         }
 
         // Dot
-        if (points_added.size() == 1) {
-            Point p = points_added[0];
+        if (points_buffer.size() == 1) {
+            Point p = points_buffer[0];
             shapes.push_back(Dot(p[0], p[1]));
         }
 
         // Line
-        if (points_added.size() == 2) {
-            Point a = points_added[0];
-            Point b = points_added[1];
+        if (points_buffer.size() == 2) {
+            Point a = points_buffer[0];
+            Point b = points_buffer[1];
             shapes.push_back(Line(a, b));
         }
 
         // Polygon
-        if (points_added.size() > 2) {
-            shapes.push_back(Polygon(points_added));
+        if (points_buffer.size() > 2) {
+            Polygon p(points_buffer);
+            shapes.push_back(p);
         }
+
+        // Add shape name to combobox
+        combobox_shapes->append(name);
 
         // Redraw drawing area
         drawing_area->queue_draw();
 
-        // Add label to list
-        std::string name = text_input_name->get_text();
-        Gtk::Label* l = new Gtk::Label(name);
-        shapes_added.push_back(l);
-        list_shapes->add(*l);
-        l->show();
+        return true;
     }
 
+    /**
+     * @brief Cancel the adding of shape.
+     *
+     * @details Clear all the inputs and buffers and hides the dialog.
+     */
     void cancel() {
         // Clear everything
         text_input_name->set_text("");
         text_input_x->set_text("");
         text_input_y->set_text("");
-        text_points_added->get_buffer()->set_text("");
+        clear_labels();
 
         // Hide dialog
         dialog_input->hide();
 
         // Clear points
-        points_added.clear();
+        points_buffer.clear();
     }
 
+    /**
+     * @brief Add shape to memory and close the dialog.
+     *
+     * @details Will only close the dialog and add the shape if add_shape()
+     * returns true.
+     */
     void finish() {
-        add_shape();
-        cancel();
+        if (add_shape()) {
+            cancel();
+        }
     }
 
-    std::vector<Point> points_added;
+    // Hold created points
+    std::vector<Point> points_buffer;
+
+    // Hold created points labels
+    std::vector<Gtk::Label*> points_labels_buffer;
+
+    // Shapes reference
     std::vector<Shape>& shapes;
 
-    std::vector<Gtk::Label*>& shapes_added;
-
-    Gtk::Box* list_shapes = nullptr;
-
+    // Top-level dialog
     Gtk::Dialog* dialog_input = nullptr;
 
+    // Combobox
+    Gtk::ComboBoxText* combobox_shapes = nullptr;
+
+    // Buttons
     Gtk::Button* button_add_shape = nullptr;
     Gtk::Button* button_finish = nullptr;
     Gtk::Button* button_cancel = nullptr;
     Gtk::Button* button_add_point = nullptr;
 
+    // Text entries
     Gtk::Entry* text_input_name = nullptr;
     Gtk::Entry* text_input_x = nullptr;
     Gtk::Entry* text_input_y = nullptr;
 
-    Gtk::TextView* text_points_added = nullptr;
+    // Hold labels of the points added
+    Gtk::Box* box_points_added = nullptr;
 
+    // Drawin area to update when adding new shapes
     Gtk::DrawingArea* drawing_area = nullptr;
 
 protected:
+    /**
+     * @brief Get point.
+     * 
+     * @details Gets the input from the user and convert to doubles.
+     * 
+     * @return Point inputted by user.
+     * 
+     * @throws std::invalid_argument If the input is "wrong". Wrong in the
+     * sense of std::stod(double) not accepting it.
+     */
     Point get_point() {
         // Get text input
         std::string sx = text_input_x->get_text();
@@ -151,6 +217,12 @@ protected:
         return Point(x, y);
     }
 
+    /**
+     * @brief Connects all buttons to it's functions.
+     * 
+     * @details Will connect all the button signals to the methods they are
+     * supposed to call.
+     */
     void connect_buttons() {
         button_add_shape
             ->signal_clicked()
@@ -166,11 +238,34 @@ protected:
             .connect(sigc::mem_fun(*this, &Control::Dialog::add_point));
     }
 
+    /**
+     * @brief Clear labels vector.
+     * 
+     * @details The labels must be initialized with new when adding them to the
+     * vector. So this method is just to clean them up. It also removes all the
+     * references from the box_points_added widget by calling remove() on each
+     * one of the labels in it.
+     */
+    void clear_labels() {
+        for (int i = 0; i < points_labels_buffer.size(); i++) {
+            box_points_added->remove(*points_labels_buffer[i]);
+            delete points_labels_buffer[i];
+        }
+        points_labels_buffer.clear();
+    }
+
+    /**
+     * @brief Updates box with added points.
+     * 
+     * @details Simply adds a label with the point to the box.
+     * 
+     * @param p Point to be added.
+     */
     void update_added_points(Point& p) {
-        std::string s = text_points_added->get_buffer()->get_text();
-        s.append("\n");
-        s.append(p.to_string());
-        text_points_added->get_buffer()->set_text(s);
+        Gtk::Label* l = new Gtk::Label(p.to_string());
+        points_labels_buffer.push_back(l);
+        box_points_added->add(*l);
+        l->show();
     }
 
 };
