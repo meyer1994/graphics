9932128b1313a5cc78a9c175da35f68f6f3adcc4
Joao Meyer <1994meyer@gmail.com>
2018-03-17 13:10:56 -0300

    Changed point interface to be completely generic and documented it

--- src/mode/point.h
+++ src/mode/point.h
@@ -15,67 +15,181 @@ const double MATH_PI = std::acos(-1);
 
 class Point : public Vector {
 public:
+    /**
+     * @brief Default constructor.
+     * 
+     * @details Instantiate an empty point, with no coordinates whatsoever.
+     */
     Point() : Vector() {}
 
+    /**
+     * @brief Explicit constructor.
+     * 
+     * @details Creates a point with the coordinates passed into as a vector.
+     * This means, coords[0] is X, coords[1] is Y and so on. The dimension of
+     * the point is determined by the size of this vector.
+     * 
+     * @param coords Vector of doubles to be used as coordinates in the space.
+     */
     explicit Point(Vector coords) : Vector(coords) {
-        push_back(1);
-
         if (coords.size() < 2) {
             throw std::invalid_argument("Point must have, at least, 2 dimensions");
         }
     }
 
-    // 2D point
+    /**
+     * @brief 2D point constructor.
+     * 
+     * @details Utility constructor to easily create a 2D point. Is the same as
+     * calling Point(Vector{x, y}).
+     * 
+     * @param x X coordinate in space.
+     * @param y Y coordinate in space
+     */
     Point(double x, double y) : Vector() {
         push_back(x);
         push_back(y);
-        push_back(1);
     }
 
-    int dimensions() const {
-        return size() - 1;
-    }
-
+    /**
+     * @brief Scale the point from the origin.
+     * 
+     * @details Will multiply all the point coordinates by the desired ratio.
+     * A consequence of this approach is that it will make points get further
+     * away from the origin, (0, 0), when used.
+     * 
+     * @param ratio By how much do you want to scale the point's coordinates.
+     */
     void scale(double ratio) {
-        Vector r(dimensions(), ratio);
+        Vector r(size(), ratio);
         transform(scale_matrix(r));
     }
 
+    /**
+     * @brief Rotate the point based on the origin.
+     * 
+     * @details Rotate the point by the angle passed. It follows the principle
+     * of the unit circle. Which means, if you rotate it 90 degrees, it will
+     * rotate it in an anti-clockwise fashion.
+     * 
+     * Example:
+     * ```{.cpp}
+     * Point p(5, 0);
+     * p.rotate(90);
+     * // p is now (0, 5)
+     * ```
+     * 
+     * @param angle The angle, in degrees,you want to rotate.
+     */
     void rotate(double angle) {
-        transform(rotate_matrix(dimensions(), angle));
+        transform(rotate_matrix(angle));
     }
 
-    void translate(double x, double y) {
-        Vector t{x, y};
-        transform(translate_matrix(t));
+    /**
+     * @brief Translate point.
+     * 
+     * @details Will simply sum the values passed with the correspondend
+     * coordiante values.
+     * 
+     * @param v Vector with the values to be summed with the coordinates.
+     */
+    void translate(Vector v) {
+        transform(translate_matrix(v));
     }
 
+    /**
+     * @brief Transform function.
+     * 
+     * @details Applies the transformation matrix passed to the point. Very
+     * useful when there is the need for more than one transformation at one
+     * time. It simply multiplies the point with the matrix passed as argument.
+     * 
+     * @param m Matrix to transform point with.
+     */
     void transform(Matrix m) {
-        Vector v;
+        // Temporary point to store new values
+        Point temp(Vector(size() + 1, 0));
 
-        // Apply transformation matrix to point
+        // Add 1 to end of point for easier transformations
+        push_back(1);
+
+        // Apply transformation matrix to point (multiply the matrices)
         for (int i = 0; i < size(); i++) {
-            double value = 0;
             for (int j = 0; j < size(); j++) {
-                value = value + (at(j) * m[j][i]);
+                temp[i] += (at(j) * m[j][i]);
             }
-            v.push_back(value);
         }
 
         // Copy new values to this point
-        for (int i = 0; i < size(); i++) {
-            at(i) = v[i];
-        }
+        *this = temp;
+
+        // Remove 1 from end
+        pop_back();
     }
 
-    static Matrix translate_matrix(Vector& v) {
+    /**
+     * @brief Static scale matrix method.
+     * 
+     * @details Returns an scale matrix to be used in transform operations.
+     * 
+     * @param v Vector containing the values to scale dimensions.
+     * 
+     * @return Matrix of size `v.size() + 1`, of the transformation.
+     */
+    static Matrix scale_matrix(Vector v) {
         int total = v.size();
 
-        // Creates TOTAL x TOTAL matrix
+        // Matrix of zeroes
+        Matrix matrix(total + 1, Vector(total + 1, 0));
+
+        // Add ratios to main diagonal
+        for (int i = 0; i < total; i++) {
+            matrix[i][i] = v[i];
+        }
+
+        // Add 1 to last cell
+        matrix[total][total] = 1;
+
+        return matrix;
+    }
+
+    /**
+     * @brief Static rotate matrix method (only works for 2D).
+     * 
+     * @details Returns an rotate matrix to be used in transform operations.
+     * 
+     * @param angle Angle to rotate matrix from origin.
+     * 
+     * @return Matrix of size `(3 x 3)`, of the transformation.
+     */
+    static Matrix rotate_matrix(double angle) {
+        // cos() and sin() are in radians
+        double c = std::cos((-angle * MATH_PI) / 180);
+        double s = std::sin((-angle * MATH_PI) / 180);
+        return Matrix{
+            Vector{c, -s, 0},
+            Vector{s,  c, 0},
+            Vector{0,  0, 1}
+        };
+    }
+
+    /**
+     * @brief Static translate matrix method.
+     * 
+     * @details Returns an translate matrix to be used in transform operations.
+     * 
+     * @param v Vector containing the values to translate each coordinate.
+     * 
+     * @return Matrix of size `v.size() + 1`, of the transformation.
+     */
+    static Matrix translate_matrix(Vector v) {
+        int total = v.size();
+
+        // Matrix of zeroes
         Matrix matrix(total + 1, Vector(total + 1, 0));
 
         // Add 1s to main diagonal
-        for (int i = 0; i < total + 1; i++) {
+        for (int i = 0; i < total; i++) {
             matrix[i][i] = 1;
         }
 
@@ -84,56 +198,23 @@ public:
             matrix[total][i] = v[i];
         }
 
-        return matrix;
-    }
-
-    static Matrix scale_matrix(Vector& v) {
-        int total = v.size();
-
-        // Creates TOTAL x TOTAL matrix
-        Matrix matrix(total + 1);
-        for (Vector& v : matrix) {
-            v = Vector(total + 1, 0);
-        }
-
-        // Add ratios to main diagonal
-        for (int i = 0; i < total; i++) {
-            matrix[i][i] = v[i];
-        }
-
         matrix[total][total] = 1;
 
         return matrix;
     }
 
-    static Matrix rotate_matrix(int dim, double angle) {
-        if (dim > 3) {
-            throw std::invalid_argument("Rotations for more than 3 dimensions not supported");
-        }
-
-        if (dim < 2) {
-            throw std::invalid_argument("Rotations for less than 2 dimensions not supported");
-        }
-
-        // 2 dimensions
-        if (dim == 2) {
-            double c = std::cos((angle * MATH_PI) / 180);
-            double s = std::sin((angle * MATH_PI) / 180);
-            return Matrix{
-                Vector{c, -s, 0},
-                Vector{s,  c, 0},
-                Vector{0,  0, 1}
-            };
-        }
-
-        // 3 dimensions
-        throw std::invalid_argument("TODO 3 dimensional rotation");
-    }
-
+    /**
+     * @brief To string method.
+     * 
+     * @details Makes a representation of the point. For easier debugging.
+     * 
+     * @return String in the form of `Point(x, y, ...)`.
+     */
     std::string to_string() const {
         if (size() == 0) {
             return "Point()";
         }
+
         std::string str = "Point(";
         for (int i = 0; i < size() - 1; i++) {
             str.append(std::to_string(at(i)));
